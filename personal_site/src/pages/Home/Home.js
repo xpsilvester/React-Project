2011d0ff12c486537418cc4041249ec2430d028a
xpsilvester <xpsilvester@gmail.com>
2018-02-23 11:29:51 +0800

    add content

--- personal_site/src/pages/Home/Home.js
+++ personal_site/src/pages/Home/Home.js
@@ -34,10 +34,54 @@ class Nav extends Component{
 	}
 }
 
+class Banner extends Component{
+	render(){
+	  return(
+	    <div className="banner">
+	    	<h3>React</h3>
+	    	<h4>用于构建用户界面的 JavaScript 库</h4>
+	    </div>
+	  )
+	}
+}
+class Content extends Component{
+	render(){
+	  return(
+	    <div className="content">
+	    	<h4>声明式</h4>
+	    	<p>React可以轻松创建交互式用户界面。为应用程序中的每个状态设计简单的视图，当数据更改时，React 将高效地更新和正确的渲染组件。</p>
+	    	<p>声明式视图使您的代码更具可预测性，更易于调试。</p>
+	    	<h4>组件化</h4>
+	    	<p>创建好拥有各自 state(状态) 的组件，再将其组合构成更加复杂的UI界面。</p>
+	    	<p>由于组件逻辑是用 JavaScript 而不是模板编写的，因此可以通过应用程序轻松传递丰富的数据，并将 state(状态) 保留在DOM之外。</p>
+	    </div>
+	  )
+	}
+}
+class Footer extends Component{
+	render(){
+	  return(
+	    <div className="footer">
+	    	<p>Copyright © 2018 Facebook Inc.</p>
+	    	<ul>
+	    		<li>DOCS</li>
+	    		<li>安装</li>
+	    		<li>快速入门</li>
+	    		<li>高级指南</li>
+	    		<li>参考</li>
+	    	</ul>
+	    </div>
+	  )
+	}
+}
 export default class Home extends Component {
     render() {
         return (
-            <Nav />
+        	<div>
+        		<Banner />
+        		<Content />
+        		<Footer />
+        	</div> 
         )
     }
 }
--- personal_site/src/router/router.js
+++ personal_site/src/router/router.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 
 import Home from '../pages/Home/Home';
 import Page1 from '../pages/Page1/Page1';
-
+import Community from '../pages/Community/Community';
+import Blog from '../pages/Blog/Blog';
 
 const getRouter = () => (
     <Router>
         <div>
-            <ul>
-                <li><Link to="/">首页</Link></li>
-                <li><Link to="/page1">Page1</Link></li>
-            </ul>
+            <div className="nav">
+                <div className="navLeft">
+                  <p><Link to="/">首页</Link></p>
+                  <p><Link to="/page1">Page1</Link></p>
+                  <p><Link to="/Community">社区</Link></p>
+                  <p><Link to="/Blog">博客</Link></p>
+                </div>
+                <div className="navRight">
+                  <p>搜索文档</p>
+                  <p>v16.2.0</p>
+                  <p><a href="https://github.com/facebook/react/" target="_blank">Github</a></p>
+                </div>
+            </div>
             <Switch>
                 <Route exact path="/" component={Home}/>
                 <Route path="/page1" component={Page1}/>
+                <Route path="/Community" component={Community}/>
+                <Route path="/Blog" component={Blog}/>
             </Switch>
         </div>
     </Router>
