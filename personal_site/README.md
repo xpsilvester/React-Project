7cec7f7b3f2c10b2e18df332cbd7edbf283cf36e
xpsilvester <xpsilvester@gmail.com>
2018-02-26 11:36:57 +0800

    add express

--- personal_site/README.md
+++ personal_site/README.md
@@ -1,6 +1,8 @@
 # personal_site
 学习React过程中的一些页面组装练习
 
-##### 1.开发端运行，先npm install 下载所需要的依赖包，然后npm start打开浏览器localhost:8080调试
-##### 2.在服务器直接运行，直接把dist目录中的两个文件丢到服务器上直接打开
+##### npm install 下载所需要的依赖包
+##### 1.开发端运行，然后npm start打开浏览器localhost:8080调试
+##### 2.在node服务器上运行,运行node index,然后打开浏览器localhost:3000
+##### 3.在服务器直接运行，npm dev-build打包完成后，直接把dist目录中的两个文件丢到服务器上直接打开
 
---
+++ personal_site/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var webpack = require('webpack');
+var WebpackHotMiddleware = require('webpack-hot-middleware');
+var WebpackDevMiddleware = require('webpack-dev-middleware');
+var config = require('./webpack.dev.config.js');
+var compiler = webpack(config);
+var app = express();
+
+// 引入webpack组件
+app.use(WebpackDevMiddleware(compiler, {
+  publicPath: config.output.publicPath,
+  stats: { colors: true }
+}));
+app.use(WebpackHotMiddleware(compiler));
+
+var router = express.Router();
+router.get('/', function (req, res, next) {
+  res.sendfile('./dist/index.html'); // 发送静态文件
+});
+router.get('/login', function (req, res, next) {
+  res.sendfile('./dist/login_register.html'); // 发送静态文件
+});
+router.get('/register', function (req, res, next) {
+  res.sendfile('./dist/login_register.html');
+});
+
+// 根目录下的所有404错误，引向index.html页面，让react-router进行解析
+router.get('/*', function(req, res){
+  res.sendfile('./dist/index.html');
+});
+app.use(router);
+
+app.listen(3000, function () {
+  console.log('Listening on 3000');
+});
