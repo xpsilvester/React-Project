d6f36a772d1931b0eec587cbf607067800c04933
xpsilvester <xpsilvester@gmail.com>
2018-02-28 09:56:20 +0800

    modify

--- personal_site/webpack.dev.config.js
+++ personal_site/webpack.dev.config.js
@@ -5,7 +5,10 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 module.exports = {
  
     /*入口*/
-    entry: path.join(__dirname, 'src/index.js'),
+    entry: [
+        'react-hot-loader/patch',
+        path.join(__dirname, 'src/index.js')
+    ],
     /*src文件夹下面的以.js结尾的文件，要使用babel解析*/
  	/*cacheDirectory是用来缓存编译结果，下次编译加速*/
 	module: {
@@ -39,11 +42,12 @@ module.exports = {
     },
     devServer: {
         port: 8080,
-        contentBase: path.join(__dirname, './dist'),
+        contentBase: path.join(__dirname, './public'),
         historyApiFallback: true,
         host: '0.0.0.0'
     },
     plugins: [
-        new UglifyJSPlugin()
+        new UglifyJSPlugin(),
+        new webpack.HotModuleReplacementPlugin()
     ]
 };
